589490e3b8e0e916b56793acb0c462be917b044a
Farhan Jamil <71662961+FarhanJ2@users.noreply.github.com>
2026-07-13 20:32:44 -0400

    create gradient

---
+++ sdf_render.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+
+float lerp(float c, float c_start, float c_end, float steps) {
+    if (steps == 0.0f) return c_start;
+    float t = c / steps;
+    // C = c_start + t * (c_end - c_start)
+    return c_start + t * (c_end - c_start);
+    
+}
+
+int main() {
+    const int width = 1280;
+    const int height = 720;
+    int total = 0;
+    FILE *fptr = fopen("output.ppm", "w");
+
+    if (fptr != NULL) {
+        fprintf(fptr, "P3\n%d %d\n255\n", width, height);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                total +=1;
+                fprintf(fptr, "%d %d %d\n", (int)lerp(total, 255, 0, width * height), 0, (int)lerp(total, 0, 255, (float)(width * height)));
+            }
+        }
+        fclose(fptr);
+        return 1;
+    }
+    printf("Failed to create file.");
+    return 0;
+}
